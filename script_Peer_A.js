3bcb46660b014ed391256829d1c7ba91d961f707
Shivam Singla <shivamsingla491@gmail.com>
2022-06-04 12:03:16 +0530

    Added Video and Chat

--- script_Peer_A.js
+++ script_Peer_A.js
@@ -1,117 +1,134 @@
 const offerBox = document.querySelector("#local_address");
-        const answerBox = document.querySelector("#remote_address");
-        const inBox = document.querySelector("#incoming");
-        const outBox = document.querySelector("#outgoing");
-        const confirmButton = document.querySelector(".accept_answer");
-        
+const answerBox = document.querySelector("#remote_address");
+const inBox = document.querySelector("#incoming");
+const outBox = document.querySelector("#outgoing");
+const confirmButton = document.querySelector(".accept_answer");
 
+const configuration = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+    //      {
+    //       'urls': "turn:openrelay.metered.ca:80",
+    //       'username': "openrelayproject",
+    //       'credential': "openrelayproject",
+    //      },
+    //     {
+    //       'urls': "turn:openrelay.metered.ca:443",
+    //       'username': "openrelayproject",
+    //       'credential': "openrelayproject",
+    //     },
+    //     {
+    //       'urls': "turn:openrelay.metered.ca:443?transport=tcp",
+    //       'username': "openrelayproject",
+    //       'credential': "openrelayproject",
+    //     },
+    //     {
+    //     'url': 'turn:numb.viagenie.ca',
+    //     'credential': 'muazkh',
+    //     'username': 'webrtc@live.com'
+    // },
+    // {
+    //     'url': 'turn:192.158.29.39:3478?transport=udp',
+    //     'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    //     'username': '28224511:1379330808'
+    // },
+    // {
+    //     'url': 'turn:192.158.29.39:3478?transport=tcp',
+    //     'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    //     'username': '28224511:1379330808'
+    // },
+    // {
+    //     'url': 'turn:turn.bistri.com:80',
+    //     'credential': 'homeo',
+    //     'username': 'homeo'
+    //  },
+    //  {
+    //     'url': 'turn:turn.anyfirewall.com:443?transport=tcp',
+    //     'credential': 'webrtc',
+    //     'username': 'webrtc'
+    // }
+  ],
+};
+const localConnection = new RTCPeerConnection(configuration);
 
-        const configuration = { 'iceServers': [
-            {
-                'urls': 'stun:stun.l.google.com:19302'
-            },
-        //      {
-        //       'urls': "turn:openrelay.metered.ca:80",
-        //       'username': "openrelayproject",
-        //       'credential': "openrelayproject",
-        //      },
-        //     {
-        //       'urls': "turn:openrelay.metered.ca:443",
-        //       'username': "openrelayproject",
-        //       'credential': "openrelayproject",
-        //     },
-        //     {
-        //       'urls': "turn:openrelay.metered.ca:443?transport=tcp",
-        //       'username': "openrelayproject",
-        //       'credential': "openrelayproject",
-        //     },
-        //     {
-        //     'url': 'turn:numb.viagenie.ca',
-        //     'credential': 'muazkh',
-        //     'username': 'webrtc@live.com'
-        // },
-        // {
-        //     'url': 'turn:192.158.29.39:3478?transport=udp',
-        //     'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        //     'username': '28224511:1379330808'
-        // },
-        // {
-        //     'url': 'turn:192.158.29.39:3478?transport=tcp',
-        //     'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        //     'username': '28224511:1379330808'
-        // },
-        // {
-        //     'url': 'turn:turn.bistri.com:80',
-        //     'credential': 'homeo',
-        //     'username': 'homeo'
-        //  },
-        //  {
-        //     'url': 'turn:turn.anyfirewall.com:443?transport=tcp',
-        //     'credential': 'webrtc',
-        //     'username': 'webrtc'
-        // }
-          ] };
-        const localConnection = new RTCPeerConnection(configuration);
+// adding event listeners for icecandidate
+const iceCandidates = [];
+localConnection.addEventListener("icecandidate", (event) => {
+  if (event.candidate) iceCandidates.push(event.candidate);
+  else
+    setTimeout(() => {
+      console.log("icecandidate search complete");
+      offerBox.value = JSON.stringify({
+        description: localConnection.localDescription,
+        icecandidates: iceCandidates,
+      });
+      offerBox.setAttribute("readonly", "true");
+    }, 100);
+});
 
-        // adding event listeners for icecandidate
-        const iceCandidates = [];
-        localConnection.addEventListener('icecandidate', (event) => {
-            if (event.candidate)
-                iceCandidates.push(event.candidate);
-            else setTimeout(() => {
-                console.log('icecandidate search complete');
-                offerBox.value = JSON.stringify({
-                    description: localConnection.localDescription,
-                    icecandidates: iceCandidates
-                });
-                offerBox.setAttribute('readonly', 'true');
-            }, 100);
-        });
+// creating the local videochannel
+const constraint = {
+  audio: false,
+  video: true,
+};
+navigator.mediaDevices
+  .getUserMedia(constraint)
+  .then((stream) => {
+    outBox.srcObject = stream;
+    stream.getTracks().forEach((track) => {
+      localConnection.addTrack(track, stream);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    alert("Some error occured!!!");
+  });
 
-        // creating the local videochannel
-        const constraint = {
-            audio: true,
-            video: true,
-        };
-        navigator.mediaDevices.getUserMedia(constraint)
-            .then((stream) => {
-                outBox.srcObject = stream;
-                stream.getTracks().forEach(track => {
-                    localConnection.addTrack(track, stream);
-                })
-            }).catch((err) => {
-                console.log(err);
-                alert("Some error occured!!!");
-            });
+// capturing the remote videochannel
+localConnection.addEventListener("track", async (event) => {
+  console.log("Sender: media channel opened");
+  const [remoteStream] = event.streams;
+  inBox.srcObject = remoteStream;
+});
+const sendChannel = localConnection.createDataChannel("sendChannel");
+console.log("channel made");
+sendChannel.onmessage = (e) =>
+  console.log("Message Recieved From Device B : " + e.data);
+sendChannel.onopen = (e) => {
+  console.log("Communication Established Now you can Chat !!!");
+  document.querySelector(".send_response").disabled = false;
+};
+sendChannel.onclose = (e) => console.log("closed!!!!!!");
+setTimeout(() => {
+  // creating an offer for the new datachannel
 
-        // capturing the remote videochannel
-        localConnection.addEventListener('track', async (event) => {
-            console.log('Sender: media channel opened');
-            const [remoteStream] = event.streams;
-            inBox.srcObject = remoteStream;
-        });
+  localConnection
+    .createOffer()
+    .then((offer) => {
+      localConnection.setLocalDescription(offer);
+    })
+    .then(() => {
+      console.log("Sender: offer initiated");
+    });
+}, 1000);
+confirmButton.onclick = (event) => {
+  const { description, icecandidates } = JSON.parse(answerBox.value);
+  answerBox.setAttribute("readonly", "true");
 
-        setTimeout(()=>{
-            // creating an offer for the new datachannel
-            localConnection.createOffer().then(offer => { localConnection.setLocalDescription(offer) }).then(() => { console.log("Sender: offer initiated") });
-        },1000)
-        confirmButton.onclick = (event) => {
-            const { description, icecandidates } = JSON.parse(answerBox.value);
-            answerBox.setAttribute('readonly', 'true');
+  // accepting the offer
+  localConnection.setRemoteDescription(description).then(() => {
+    console.log("Sender: answer accepted");
+  });
 
-            // accepting the offer
-            localConnection.setRemoteDescription(description).then(() => { console.log("Sender: answer accepted") });
+  // adding proposed icecandidates
+  icecandidates.forEach((candidate) => {
+    localConnection.addIceCandidate(new RTCIceCandidate(candidate));
+  });
+};
 
-            // adding proposed icecandidates
-            icecandidates.forEach(candidate => {
-                localConnection.addIceCandidate(new RTCIceCandidate(candidate));
-            });
-
-            confirmButton.disabled = true;
-        };
-
-
-        (function () {
+(function () {
   var old = console.log;
   var logger = document.querySelector(".chat");
   console.log = function (message) {
@@ -123,3 +140,11 @@ const offerBox = document.querySelector("#local_address");
     }
   };
 })();
+document.querySelector(".send_response").addEventListener("click", async () => {
+  const response = document.getElementById("chat_text").value;
+  const text = document.createElement("div");
+  text.innerHTML =
+    "Message Sent By You : " + document.getElementById("chat_text").value;
+  document.querySelector(".chat").appendChild(text);
+  sendChannel.send(response);
+});
--- script_Peer_B.js
+++ script_Peer_B.js
@@ -1,113 +1,134 @@
 const offerBox = document.querySelector("#remote_address");
-        const answerBox = document.querySelector("#local_address");
-        const inBox = document.querySelector("#incoming");
-        const outBox = document.querySelector("#outgoing");
-        const confirmButton = document.querySelector(".offer_entered");
+const answerBox = document.querySelector("#local_address");
+const inBox = document.querySelector("#incoming");
+const outBox = document.querySelector("#outgoing");
+const confirmButton = document.querySelector(".offer_entered");
 
-       
-        const configuration = { 'iceServers': [
-            {
-                'urls': 'stun:stun.l.google.com:19302'
-            },
-        //      {
-        //       'urls': "turn:openrelay.metered.ca:80",
-        //       'username': "openrelayproject",
-        //       'credential': "openrelayproject",
-        //      },
-        //     {
-        //       'urls': "turn:openrelay.metered.ca:443",
-        //       'username': "openrelayproject",
-        //       'credential': "openrelayproject",
-        //     },
-        //     {
-        //       'urls': "turn:openrelay.metered.ca:443?transport=tcp",
-        //       'username': "openrelayproject",
-        //       'credential': "openrelayproject",
-        //     },
-        //     {
-        //     'url': 'turn:numb.viagenie.ca',
-        //     'credential': 'muazkh',
-        //     'username': 'webrtc@live.com'
-        // },
-        // {
-        //     'url': 'turn:192.158.29.39:3478?transport=udp',
-        //     'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        //     'username': '28224511:1379330808'
-        // },
-        // {
-        //     'url': 'turn:192.158.29.39:3478?transport=tcp',
-        //     'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        //     'username': '28224511:1379330808'
-        // },
-        // {
-        //     'url': 'turn:turn.bistri.com:80',
-        //     'credential': 'homeo',
-        //     'username': 'homeo'
-        //  },
-        //  {
-        //     'url': 'turn:turn.anyfirewall.com:443?transport=tcp',
-        //     'credential': 'webrtc',
-        //     'username': 'webrtc'
-        // }
-          ] };
-        const localConnection = new RTCPeerConnection(configuration);
+const configuration = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+    //      {
+    //       'urls': "turn:openrelay.metered.ca:80",
+    //       'username': "openrelayproject",
+    //       'credential': "openrelayproject",
+    //      },
+    //     {
+    //       'urls': "turn:openrelay.metered.ca:443",
+    //       'username': "openrelayproject",
+    //       'credential': "openrelayproject",
+    //     },
+    //     {
+    //       'urls': "turn:openrelay.metered.ca:443?transport=tcp",
+    //       'username': "openrelayproject",
+    //       'credential': "openrelayproject",
+    //     },
+    //     {
+    //     'url': 'turn:numb.viagenie.ca',
+    //     'credential': 'muazkh',
+    //     'username': 'webrtc@live.com'
+    // },
+    // {
+    //     'url': 'turn:192.158.29.39:3478?transport=udp',
+    //     'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    //     'username': '28224511:1379330808'
+    // },
+    // {
+    //     'url': 'turn:192.158.29.39:3478?transport=tcp',
+    //     'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    //     'username': '28224511:1379330808'
+    // },
+    // {
+    //     'url': 'turn:turn.bistri.com:80',
+    //     'credential': 'homeo',
+    //     'username': 'homeo'
+    //  },
+    //  {
+    //     'url': 'turn:turn.anyfirewall.com:443?transport=tcp',
+    //     'credential': 'webrtc',
+    //     'username': 'webrtc'
+    // }
+  ],
+};
+const remoteConnection = new RTCPeerConnection(configuration);
 
-        // adding event listeners for icecandidate
-        const iceCandidates = [];
-        localConnection.addEventListener('icecandidate', (event) => {
-            if (event.candidate)
-                iceCandidates.push(event.candidate);
-            else setTimeout(() => {
-                console.log('icecandidate search complete');
-                answerBox.value = JSON.stringify({
-                    description: localConnection.localDescription,
-                    icecandidates: iceCandidates
-                });
-                answerBox.setAttribute('readonly', 'true');
-            }, 100);
-        });
+// adding event listeners for icecandidate
+const iceCandidates = [];
+remoteConnection.addEventListener("icecandidate", (event) => {
+  if (event.candidate) iceCandidates.push(event.candidate);
+  else
+    setTimeout(() => {
+      console.log("icecandidate search complete");
+      answerBox.value = JSON.stringify({
+        description: remoteConnection.localDescription,
+        icecandidates: iceCandidates,
+      });
+    }, 100);
+});
 
-        // creating the local videochannel
-        const constraint = {
-            audio: true,
-            video: true,
-        };
-        navigator.mediaDevices.getUserMedia(constraint)
-            .then((stream) => {
-                outBox.srcObject = stream;
-                stream.getTracks().forEach(track => {
-                    localConnection.addTrack(track, stream);
-                })
-            }).catch((err) => {
-                console.log(err);
-                alert("Some error occured!!!");
-            });
+// creating the local videochannel
+const constraint = {
+  audio: false,
+  video: true,
+};
+navigator.mediaDevices
+  .getUserMedia(constraint)
+  .then((stream) => {
+    outBox.srcObject = stream;
+    stream.getTracks().forEach((track) => {
+      remoteConnection.addTrack(track, stream);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    alert("Some error occured!!!");
+  });
 
-        // capturing the remote videochannel
-        localConnection.addEventListener('track', async (event) => {
-            console.log('Receiver: media channel opened');
-            const [remoteStream] = event.streams;
-            inBox.srcObject = remoteStream;
-        });
+// capturing the remote videochannel
+remoteConnection.addEventListener("track", async (event) => {
+  console.log("Receiver: media channel opened");
+  const [remoteStream] = event.streams;
+  inBox.srcObject = remoteStream;
+});
 
-        confirmButton.onclick = (event) => {
-            const { description, icecandidates } = JSON.parse(offerBox.value);
-            offerBox.setAttribute('readonly', 'true');
+confirmButton.onclick = (event) => {
+  const { description, icecandidates } = JSON.parse(offerBox.value);
+  offerBox.setAttribute("readonly", "true");
 
-            // accepting the offer
-            localConnection.setRemoteDescription(description).then(() => { console.log("Receiver: offer accepted") });
+  // accepting the offer
+  remoteConnection.setRemoteDescription(description).then(() => {
+    console.log("Receiver: offer accepted");
+  });
 
-            // adding proposed icecandidates
-            icecandidates.forEach(candidate => {
-                localConnection.addIceCandidate(new RTCIceCandidate(candidate));
-            });
-            localConnection.createAnswer().then((answer) => localConnection.setLocalDescription(answer)).then(() => { console.log("Receiver: answer initiated") });
+  // adding proposed icecandidates
+  icecandidates.forEach((candidate) => {
+    remoteConnection.addIceCandidate(new RTCIceCandidate(candidate));
+  });
+  var receiveChannel;
+  remoteConnection.ondatachannel = (e) => {
+    console.log("b pe channel dhundo");
+    receiveChannel = e.channel;
+    receiveChannel.onmessage = (e) =>
+      console.log("Message Recieved From Device A : " + e.data);
+    receiveChannel.onopen = (e) => {
+      console.log("Communication Established Now you can Chat !!!");
+      document.querySelector(".send_response").disabled = false;
+    };
+    receiveChannel.onclose = (e) => {
+      console.log("closed!!!!!!");
+    };
+    remoteConnection.channel = receiveChannel;
+  };
+  remoteConnection
+    .createAnswer()
+    .then((answer) => remoteConnection.setLocalDescription(answer))
+    .then(() => {
+      console.log("Receiver: answer initiated");
+    });
+};
 
-        };
-
-
-
-        (function () {
+(function () {
   var old = console.log;
   var logger = document.querySelector(".chat");
   console.log = function (message) {
@@ -119,3 +140,11 @@ const offerBox = document.querySelector("#remote_address");
     }
   };
 })();
+document.querySelector(".send_response").addEventListener("click", async () => {
+  const response = document.getElementById("chat_text").value;
+  const text = document.createElement("div");
+  text.innerHTML =
+    "Message Sent By You : " + document.getElementById("chat_text").value;
+  document.querySelector(".chat").appendChild(text);
+  remoteConnection.channel.send(response);
+});
