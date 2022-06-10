a17f22350edc3f36c9a3e4249ed5cdf5e3a43779
Shivam Singla <shivamsingla491@gmail.com>
2022-06-10 12:33:03 +0530

    TURN server added

--- script_Peer_B.js
+++ script_Peer_B.js
@@ -9,21 +9,21 @@ const configuration = {
     {
       urls: "stun:stun.l.google.com:19302",
     },
+    // {
+    //   urls: "turn:openrelay.metered.ca:80",
+    //   username: "openrelayproject",
+    //   credential: "openrelayproject",
+    // },
     {
-      urls: "turn:openrelay.metered.ca:80",
+      urls: "turn:openrelay.metered.ca:443",
       username: "openrelayproject",
       credential: "openrelayproject",
     },
-    //     {
-    //       'urls': "turn:openrelay.metered.ca:443",
-    //       'username': "openrelayproject",
-    //       'credential': "openrelayproject",
-    //     },
-    //     {
-    //       'urls': "turn:openrelay.metered.ca:443?transport=tcp",
-    //       'username': "openrelayproject",
-    //       'credential': "openrelayproject",
-    //     },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
     //     {
     //     'url': 'turn:numb.viagenie.ca',
     //     'credential': 'muazkh',
