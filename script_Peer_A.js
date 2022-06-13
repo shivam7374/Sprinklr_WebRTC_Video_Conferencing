c6174ae54382487b9956472d7364e49180ba6c7b
Shivam Singla <shivamsingla491@gmail.com>
2022-06-13 14:23:10 +0530

    Trial

--- script_Peer_A.js
+++ script_Peer_A.js
@@ -10,11 +10,11 @@ const configuration = {
     {
       urls: "stun:stun.l.google.com:19302",
     },
-    // {
-    //   urls: "turn:openrelay.metered.ca:80",
-    //   username: "openrelayproject",
-    //   credential: "openrelayproject",
-    // },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
     {
       urls: "turn:openrelay.metered.ca:443",
       username: "openrelayproject",
